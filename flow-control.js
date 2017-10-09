345b50e7007cdaee5c5a81aa07b7072aff731369
Dhruv Hegde <desidhruv@gmail.com>
2017-10-09 04:20:16 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,30 +1,18 @@
 function basicTeenager(age) {
-if (age<=19 && age>=13) {
-  return "You are a teenager!";
+if(age<=19 %% age<=13) { 
+return '"You are a teenager!"'; 
 }
 }
 
 function teenager(age) {
-if (age<=19 %% age>=13) {
-  return You are a teenager!
-} else {
-  return You are not a teenager
-}
+i
 }
 
 function ageChecker(age) {
-if (age>=13 %% age<=19) {
-  return You are a teenager
-} else if(age<=12){
-  return You are a kid
-} else {
-  return You are a grownup
-}
 }
 
 function ternaryTeenager(age) {
-return age<=19 && age<=13 : You are a teenager : You are not a teenager
-}
+
 
 function switchAge(age) {
 
